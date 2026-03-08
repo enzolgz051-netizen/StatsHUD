199d854075887ff522cfaa0903f866841ab45124
enzolgz051-netizen <enzolgz051@gmail.com>
2026-03-08 19:49:20 +0100

    Update StatsHUD.h

--- StatsHUD.h
+++ StatsHUD.h
@@ -10,7 +10,6 @@ public:
 
 private:
     void Render(CanvasWrapper canvas);
-    float GetBoostAmount();
     void OnGoalScored(std::string eventName);
     void OnMatchEnded(std::string eventName);
     void OnMatchStarted(std::string eventName);
@@ -20,9 +19,12 @@ private:
     int totalWins   = 0;
     int totalLosses = 0;
     bool lastGameWon = false;
-    bool inMatch = false;
+    bool inMatch  = false;
     bool isOrange = false;
 
     std::shared_ptr<bool>  cvarEnabled;
     std::shared_ptr<float> cvarScale;
+    std::shared_ptr<float> cvarOffsetX;  // décalage X depuis bord droit
+    std::shared_ptr<float> cvarOffsetY;  // décalage Y depuis bord bas
+    std::shared_ptr<float> cvarBoostR;   // rayon du cercle boost
 };
